99d96d964844629b510b2b46965d11a20fae879a
Caleb <calebjrbako1231@gmail.com>
2023-09-13 14:13:44 +0100

    commits

--- client/src/components/Pages/Home/FileUploader.jsx
+++ client/src/components/Pages/Home/FileUploader.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import './Home.css';
-import FileViewer from 'react-file-viewer';
-import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+import FileViewer from "react-file-viewer";
+// import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 
 export default function FileUploader({files,onChange}){   
     function uploadFiles(ev){
@@ -11,15 +11,19 @@ export default function FileUploader({files,onChange}){
             data.append('file',files[i]);
         }
         axios.post('/upload',data,{
-            headers: {'Content-type':'multipart/form-data',"Access-Control-Allow-Origin": "http://localhost:4000",}
+            headers: {
+            'Access-Control-Allow-Origin' : '*',   
+            'Content-type':'multipart/form-data',
+            Accept:"application/json"}
         }).then(response => {
             const {data:filenames} = response;
             onChange(prev => {
                 return [...prev, ...filenames];
             });
-        })
+        }).catch(err => console.log(err));
     }      
     const type = 'docx';
+    const file = 'http://example.com/image.png';
 
     return(
         <div className="fileUploader">
@@ -27,12 +31,23 @@ export default function FileUploader({files,onChange}){
                 <input type="file" multiple className="hidden" onChange={uploadFiles}/>  
                 Upload
             </label>
-            {files.length > 0 && files.map(file=>(
-                <FileViewer
-                    fileType={type}
-                    filePath={`http://localhost:4000/uploads\\${file}`}
-                />
+            {files.length > 0 && files.map(p =>(
+                <div>
+                    <img className="file-pics" src={`http://localhost:4000/uploads\\`+ p} alt="" />
+                </div>
             ))}
+                  {/* <DocViewer
+                        documents={files.map((file) => ({
+                        uri: (`http://localhost:4000/uploads\\`+ file),
+                        fileName: file.name,
+                        }))}
+                        pluginRenderers={DocViewerRenderers}
+                    /> */}
+                    
+                    {/* <FileViewer
+                        fileType={type}
+                        filePath={file}
+                    /> */}
 
         </div>
     )
--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
-    optionSuccessStatus:200
+    optionSuccessStatus:200,
 }));
 console.log("sever connected");
 mongoose.connect(mongourl);
