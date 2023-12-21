5e1b09c085109fe444d87cd63657dfc5a2bf4e8a
Caleb <calebjrbako1231@gmail.com>
2023-12-21 11:39:18 +0100

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import GenDisplay from "./components/Pages/Display/GenDisplay";
 import SingleDoc from "./components/Pages/Display/SingleDoc";
 
 
-axios.defaults.baseURL = 'https://repo-site.netlify.app';
+axios.defaults.baseURL = 'https://reposite.onrender.com';
 axios.defaults.withCredentials = true;
 
 function App() {
